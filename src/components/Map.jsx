a22abadb3e178471ba498c10062cb713381a72cf
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-07-17 00:46:29 -0700

    applied the pointer movability on map functionality by creating a custom component, detecting the co ordinate where user have click on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,53 +1,81 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
-
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
 
-  const {cities }= useCities();
+  const { cities } = useCities();
 
   const [searchParams, setSearchParams] = useSearchParams(); // its like useState hookn, whcih also give array
   const [mapPosition, setMapPaosition] = useState([100, 100]);
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  console.log(lat,lng)
+  console.log(lat, lng);
+
+  useEffect(
+    function () {
+      if (lat && lng) setMapPaosition([lat, lng]);
+    },
+    [lat, lng]
+  );
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
-      <MapContainer className={styles.mapContainer} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.mapContainer}
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        {cities.map((city) => <Marker position={[city.position.lat,city.position.lng]} key={city.id}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>) }
-        <ChangeCenter position={[lat || 100,lng || 100]}/>
-
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
 }
 
-function ChangeCenter({position})
-{
+function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
   return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => {
+      console.log(e);
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
