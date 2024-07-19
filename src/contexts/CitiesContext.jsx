7302f0f15ba04657d58a3159e1c5745ef15b736b
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-07-19 14:47:36 +0530

    implementing useReducer Hook to replace the useStates hook

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,27 +1,69 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { createContext, useState, useEffect, useContext } from "react";
+import {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  useReducer,
+} from "react";
 
 const CitiesContext = createContext();
+
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "cities/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+
+    case "cities/created":
+
+    case "cities/deleted":
+
+    default:
+      throw new Error("Unknown action type ");
+  }
+}
 
 const BASE_URL = "http://localhost:9000";
 
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState();
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ isLoading, cities, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  // const [cities, setCities] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
+      // setIsLoading(true);
+      dispatch({ type: "loading" });
       try {
-        setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         if (!res.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await res.json();
         console.log(data);
-        setCities(data);
+        // setCities(data);
+        dispatch({type : 'cities/loaded', payload : data})
         console.log(data);
       } catch (error) {
         console.error("There was an error fetching the data:", error);
@@ -80,10 +122,10 @@ function CitiesProvider({ children }) {
       await fetch(`${BASE_URL}/cities/${id}`, {
         method: "DELETE",
       });
-    
+
       // console.log(data);
       // console.log(cities)
-      setCities((cities) => cities.filter(city=> city.id !== id )); // console.log(data);
+      setCities((cities) => cities.filter((city) => city.id !== id)); // console.log(data);
     } catch (error) {
       console.error("There was an error in deleting the city:", error);
     } finally {
@@ -93,7 +135,14 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity, deleteCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
